c6fcfa1e802b8c21ee355072b7e0d92428ba24a1
Gustavo Arthur Barbosa Ulrich <gustavoarthurbarbosa@gmail.com>
2022-10-22 16:52:20 -0300

    Validação de dados e ValidationException

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -68,6 +71,9 @@ public class DepartmentFormController implements Initializable {
 			notifyDataChangeListeners();
 			Utils.currentStage(event).close();
 		}
+		catch (ValidationException e) {
+			setErrorMessages(e.getErrors());
+		}
 		catch(DbException ex) {
 			Alerts.showAlert("Error saving object", null, ex.getMessage(), AlertType.ERROR);
 		}
@@ -82,8 +88,19 @@ public class DepartmentFormController implements Initializable {
 	private Department getFormData() {
 		Department obj = new Department();
 		
+		ValidationException exception = new ValidationException("Validation error");
+		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field can't be empty");
+		}
+		
 		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		
 		return obj;
 	}
@@ -108,6 +125,15 @@ public class DepartmentFormController implements Initializable {
 			throw new IllegalStateException("Entity was null.");
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String, String> errors) {
+		//Salvando todas as chaves do map no arrajo fields
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			lblErrorName.setText(errors.get("name"));
+		}
 	}
 	
 }
